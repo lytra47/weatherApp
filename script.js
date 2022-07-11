11e18ff47a03bbbf506da93fe9d5037c732c3b5f
Sreehari S <lytra47@proton.me>
2022-07-10 22:32:12 -0700

    Added full responsiveness, added adaptive icons.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=ernakulam&appid=8ac5c4d57ba6a4b3dfcf622700447b1e"
+  "https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=8ac5c4d57ba6a4b3dfcf622700447b1e"
 )
   .then((result) => result.json())
   .then((weath) => displayData(weath));
@@ -18,13 +18,23 @@ function displayData(weath) {
     alert("City not found");
   } else {
     city.innerHTML = weath.name;
+    mainw.innerHTML = weath.weather[0].main;
     degree.innerHTML = Math.floor(weath.main.temp - 273) + `<sup>o</sup>C`;
     feels.innerHTML = Math.floor(weath.main.feels_like - 273) + `<sup>o</sup>C`;
     humid.innerHTML = weath.main.humidity;
     pressure.innerHTML = weath.main.pressure;
     wind.innerHTML = weath.wind.speed;
     vis.innerHTML = weath.visibility;
+    // ICON
+    weathICON = weath.weather[0].icon;
+    icon.innerHTML = `  <img src="http://openweathermap.org/img/wn/${weathICON}@2x.png" alt="" /> `; //  //  weath.weather[0].id;
   }
 
   // document.getElementById("result").innerHTML += htmlData;
 }
+
+loc1.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    searchw();
+  }
+});
